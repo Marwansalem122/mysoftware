62be91f2ff7bb1fc99b01296c8b6aa69403b46f6
Ziad2274 <115370562+Ziad2274@users.noreply.github.com>
2023-05-07 07:52:32 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
+import 'package:mysoftwareproject/features/Auth/presentation/pages/imodles/cart.dart';
+
 
 // import 'features/Auth/presentation/manger/cubit/auth_cubit.dart';
 import 'features/Splash/presentation/splashView.dart';
-
+import 'package:provider/provider.dart';
 void main() async {
 
-  runApp(FruitsMarket());
+  runApp(ChangeNotifierProvider(
+    create: (context)=>Cart(),
+      child: FruitsMarket()));
 }
 class FruitsMarket extends StatelessWidget {
   const FruitsMarket({Key? key}) : super(key: key);
